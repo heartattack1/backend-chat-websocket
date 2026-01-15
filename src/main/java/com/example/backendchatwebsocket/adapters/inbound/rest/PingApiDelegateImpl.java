08bca844cab7eef7b67fc9cf267d58546c7fd581
Robert S <r.sadretdinov4110@gmail.com>
2026-01-16 02:38:38 +0300

    Fixed interface implementation

--- src/main/java/com/example/backendchatwebsocket/adapters/inbound/rest/PingApiDelegateImpl.java
+++ src/main/java/com/example/backendchatwebsocket/adapters/inbound/rest/PingApiDelegateImpl.java
@@ -1,12 +1,12 @@
 package com.example.backendchatwebsocket.adapters.inbound.rest;
 
+import com.example.backendchat.api.PingApi;
 import com.example.backendchat.api.model.PingResponse;
-import com.example.backendchat.api.PingApiDelegate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PingApiDelegateImpl implements PingApiDelegate {
+public class PingApiDelegateImpl implements PingApi {
 
     @Override
     public ResponseEntity<PingResponse> getPing() {
